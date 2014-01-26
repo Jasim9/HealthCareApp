26fc151c7380a59e2a719d49f75e54566f41c4d2
Jasim9 <wajahat.lums@gmail.com>
2014-01-26 10:14:20 +0500

    Layout changes
    Home screen

--- gen/com/health/care/R.java
+++ gen/com/health/care/R.java
@@ -25,9 +25,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
+        public static final int heatMap=0x7f070003;
+        public static final int map=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +36,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050001;
-        public static final int dummy_content=0x7f050002;
+        public static final int dummy_content=0x7f050004;
+        public static final int hmap=0x7f050003;
+        public static final int map=0x7f050002;
     }
     public static final class style {
         /** 
